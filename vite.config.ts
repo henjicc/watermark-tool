d173dd0d5a5ba1f1f5c20dc3d5c413ed67219e4e
henjicc <henji_henji@qq.com>
2025-07-05 03:00:06 +0800

    修改vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   // 移除对lucide-react的排除，以避免图标文件被广告拦截器阻止
   // optimizeDeps: {
